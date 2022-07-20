4974c558818d4f7a0af3d490c52cb70275d7175e
Chris Schimetschka <chris.schimetschka@gmail.com>
2022-07-20 18:58:54 +0200

    added source code

---
+++ src/Clustering.java
@@ -0,0 +1,240 @@
+import java.util.*;
+import java.awt.Color;
+
+/**
+ * This class solves a clustering problem with the Prim algorithm.
+ */
+public class Clustering {
+	EdgeWeightedGraph G;
+	List<List<Integer>> clusters;
+	List<List<Integer>> labeled;
+	
+	/**
+	 * Constructor for the Clustering class, for a given EdgeWeightedGraph and no labels.
+	 * @param G a given graph representing a clustering problem
+	 */
+	public Clustering(EdgeWeightedGraph G) {
+		this.G = G;
+	    this.clusters = new LinkedList<>();
+	}
+	
+    /**
+	 * Constructor for the Clustering class, for a given data set with labels
+	 * @param in input file for a clustering data set with labels
+	 */
+	public Clustering(In in) {
+            int V = in.readInt();
+            int dim= in.readInt();
+            this.G = new EdgeWeightedGraph(V);
+            this.labeled = new LinkedList<>();
+            LinkedList labels = new LinkedList();
+            double[][] coord = new double [V][dim];
+            for (int v = 0;v<V; v++ ) {
+                for(int j=0; j<dim; j++) {
+                	coord[v][j]=in.readDouble();
+                }
+                String label= in.readString();
+                    if(labels.contains(label)) {
+                    	this.labeled.get(labels.indexOf(label)).add(v);
+                    }
+                    else {
+                    	labels.add(label);
+                    	List<Integer> l = new LinkedList<>();
+                    	this.labeled.add(l);
+                    	this.labeled.get(labels.indexOf(label)).add(v);
+                    	System.out.println(label);
+                    }                
+            }
+             
+            this.G.setCoordinates(coord);
+            for (int w = 0; w < V; w++) {
+                for (int v = 0;v < V; v++ ) {
+                	if (v != w) {
+                	double weight = 0;
+                    for(int j = 0; j < dim; j++) {
+                    	weight += Math.pow(G.getCoordinates()[v][j]-G.getCoordinates()[w][j],2);
+                    }
+                    weight = Math.sqrt(weight);
+                    Edge e = new Edge(v, w, weight);
+                    G.addEdge(e);
+                	}
+                }
+            }
+	    this.clusters = new LinkedList<>();
+	}
+	
+    /**
+	 * This method finds a specified number of clusters based on a MST.
+	 *
+	 * It is based in the idea that removing edges from a MST will create a
+	 * partition into several connected components, which are the clusters.
+	 * @param numberOfClusters number of expected clusters
+	 */
+	public void findClusters(int numberOfClusters){
+		// create a MST with given Graph
+		PrimMST mst = new PrimMST(this.G);
+		// create an empty list and loop over edges returned from MST
+		LinkedList<Edge> edges = new LinkedList<>();
+		for (Edge e : mst.edges()) {
+			edges.add(e);
+		}
+		// sort edges and remove edges with the highest weight
+		Collections.sort(edges);
+		for (int i = 0; i < numberOfClusters-1; i++) {
+			edges.removeLast();
+		}
+		// check for connected components and sort clusters
+		connectedComponents(edges);
+		for (List<Integer> cluster : clusters) {
+			Collections.sort(cluster);
+		}
+	}
+	
+	/**
+	 * This method finds clusters based on a MST and a threshold for the coefficient of variation.
+	 *
+	 * It is based in the idea that removing edges from a MST will create a
+	 * partition into several connected components, which are the clusters.
+	 * The edges are removed based on the threshold given. For further explanation see the exercise sheet.
+	 *
+	 * @param threshold for the coefficient of variation
+	 */
+	public void findClusters(double threshold){
+		// create a MST with given Graph
+		PrimMST mst = new PrimMST(this.G);
+		// create an empty list and loop over edges returned from MST
+		LinkedList<Edge> edges = new LinkedList<>();
+		for (Edge e : mst.edges()) {
+			edges.add(e);
+		}
+		// sort edges and remove edges where the returned coefficient is higher than the given threshold
+		Collections.sort(edges);
+		LinkedList<Edge> part = new LinkedList<>();
+		for (Edge e : edges) {
+			part.add(e);
+			double coEff = coefficientOfVariation(part);
+			if (coEff > threshold) {
+				part.remove(e);
+			}
+		}
+		// check for connected components and sort clusters
+		connectedComponents(part);
+		for (List<Integer> cluster : clusters) {
+			Collections.sort(cluster);
+		}
+	}
+	
+	/**
+	 * Evaluates the clustering based on a fixed number of clusters.
+	 * @return array of the number of the correctly classified data points per cluster
+	 */
+	public int[] validation() {
+		int[] clusterNum = new int[clusters.size()];
+		for (int c = 0; c < clusters.size(); c++) {
+			int count = 0;
+			for (int n = 0; n < labeled.get(c).size(); n++) {
+				if (clusters.get(c).contains(labeled.get(c).get(n))) {
+					count++;
+				}
+			}
+			clusterNum[c] = count;
+		}
+		return clusterNum;
+	}
+	
+	/**
+	 * Calculates the coefficient of variation.
+	 * For the formula see exercise sheet.
+	 * @param part list of edges
+	 * @return coefficient of variation
+	 */
+	public double coefficientOfVariation(List <Edge> part) {
+		if (part.isEmpty()) return 0.0;
+		return (standardDeviation(part) / average(part));
+	}
+
+	/** Source: GeeksForGeeks */
+	private double average(List<Edge> part) {
+		double result = 0;
+		for (Edge e : part) {
+			result += e.weight();
+		}
+		return result / part.size();
+	}
+
+	/** Source: GeeksForGeeks */
+	private double standardDeviation(List<Edge> part) {
+		double result = 0;
+		for (Edge e : part) {
+			result += (e.weight() - average(part)) * (e.weight() - average(part));
+		}
+		return Math.sqrt(result / (part.size()));
+	}
+
+	public void connectedComponents(List<Edge> edges) {
+		// create Union Find
+		UF uf = new UF(G.V());
+		// loop thru edges and identify representative
+		for (Edge e : edges) {
+			uf.union(e.either(), e.other(e.either()));
+		}
+		// create ArrayList to store unique representatives
+		ArrayList<Integer> reps = new ArrayList<>();
+		for (int i = 0; i < G.V(); i++) {
+			int rep = uf.find(i);
+			if (!reps.contains(rep)) {
+				reps.add(rep);
+			}
+		}
+		// create cluster sub lists based on unique representatives
+		for (int j = 0; j < reps.size(); j++) {
+			clusters.add(new ArrayList<>());
+		}
+		// add nodes based on their representatives
+		for (int k = 0; k < G.V(); k++) {
+			for (int r = 0; r < reps.size(); r++) {
+				if (uf.find(k) == reps.get(r)) {
+					clusters.get(r).add(k);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Plots clusters in a two-dimensional space.
+	 */
+	public void plotClusters() {
+		int canvas=800;
+	    StdDraw.setCanvasSize(canvas, canvas);
+	    StdDraw.setXscale(0, 15);
+	    StdDraw.setYscale(0, 15);
+	    StdDraw.clear(new Color(0,0,0));
+		Color[] colors= {new Color(255, 255, 255), new Color(128, 0, 0), new Color(128, 128, 128), 
+				new Color(0, 108, 173), new Color(45, 139, 48), new Color(226, 126, 38), new Color(132, 67, 172)};
+	    int color=0;
+		for(List <Integer> cluster: clusters) {
+			if(color>colors.length-1) color=0;
+		    StdDraw.setPenColor(colors[color]);
+		    StdDraw.setPenRadius(0.02);
+		    for(int i: cluster) {
+		    	StdDraw.point(G.getCoordinates()[i][0], G.getCoordinates()[i][1]);
+		    }
+		    color++;
+	    }
+	    StdDraw.show();
+	}
+
+    public static void main(String[] args) {
+		// FOR TESTING
+//		In inp = new In("graph_small.txt");
+		In inp = new In("iris_small.txt");
+//		EdgeWeightedGraph G = new EdgeWeightedGraph(inp);
+//		Clustering c = new Clustering(G);
+		Clustering c = new Clustering(inp);
+//		c.findClusters(3);
+		c.findClusters(0.4);
+		c.validation();
+		c.plotClusters();
+    }
+}
+
---
+++ src/EdgeWeightedGraph.java
@@ -0,0 +1,245 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+// This file (original written by Sedgewick & Wayne, see below) is adapted from
+// https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/EdgeWeightedGraph.java
+// for the use as AlgoDat programming exercise.
+// Please also refer to the description on the exercise sheet.
+
+/**
+ *  The {@code EdgeWeightedGraph} class represents an edge-weighted
+ *  graph of vertices named 0 through <em>V</em> – 1, where each
+ *  undirected edge is of type {@link Edge} and has a real-valued weight.
+ *  It supports the following two primary operations: add an edge to the graph,
+ *  iterate over all of the edges incident to a vertex. It also provides
+ *  methods for returning the number of vertices <em>V</em> and the number
+ *  of edges <em>E</em>. Parallel edges and self-loops are permitted.
+ *  By convention, a self-loop <em>v</em>-<em>v</em> appears in the
+ *  adjacency list of <em>v</em> twice and contributes two to the degree
+ *  of <em>v</em>.
+ *  <p>
+ *  This implementation uses an adjacency-lists representation, which 
+ *  is a vertex-indexed array of {@link Bag} objects.
+ *  All operations take constant time (in the worst case) except
+ *  iterating over the edges incident to a given vertex, which takes
+ *  time proportional to the number of such edges.
+ *  <p>
+ *  For additional documentation,
+ *  see <a href="https://algs4.cs.princeton.edu/43mst">Section 4.3</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+public class EdgeWeightedGraph {
+    private static final String NEWLINE = System.getProperty("line.separator");
+    private double[][] coord; //saves the coordinates of your nodes/ data points. 
+    private final int V;
+    private int E;
+    private Bag<Edge>[] adj;
+    
+    /**
+     * Initializes an empty edge-weighted graph with {@code V} vertices and 0 edges.
+     *
+     * @param  V the number of vertices
+     * @throws IllegalArgumentException if {@code V < 0}
+     */
+    public EdgeWeightedGraph(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Edge>();
+        }
+    }
+    
+    /**
+     * Initializes an empty edge-weighted graph from an input file
+     *
+     * @param  in input file - for the format see exercise sheet
+     * @throws IllegalArgumentException if {@code V < 0}
+     */
+    public EdgeWeightedGraph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        int dim = in.readInt();
+        coord = new double[V][dim];
+        if (E < 0) throw new IllegalArgumentException("Number of edges must be nonnegative");
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            validateVertex(v);
+            validateVertex(w);
+	        double weight=0;
+	        double[] coordv = new double[dim];
+	        double[] coordw = new double[dim];
+	
+	        for(int j=0; j<dim; j++) {
+		        coordv[j]=in.readDouble();
+		        coord[v][j]=coordv[j];
+            }
+	        for(int j=0; j<dim; j++) {
+		        coordw[j]=in.readDouble();
+		        coord[w][j]=coordw[j];
+	        }
+	        for(int j=0; j<dim; j++) {
+		        weight= weight+Math.pow(coordv[j]-coordw[j],2);
+	        }
+	        weight=Math.sqrt(weight);
+	        Edge e = new Edge(v, w, weight);
+	        addEdge(e);
+        }
+    }
+    
+    /**
+     * @return the coordinates of the nodes in the graph
+     */
+    public double[][] getCoordinates(){
+        return this.coord;
+    }
+    
+    public void setCoordinates(double [][]coord) {
+        this.coord = coord;
+    }
+
+    /**
+     * Initializes a new edge-weighted graph that is a deep copy of {@code G}.
+     *
+     * @param  G the edge-weighted graph to copy
+     */
+    public EdgeWeightedGraph(EdgeWeightedGraph G) {
+        this(G.V());
+        this.E = G.E();
+        for (int v = 0; v < G.V(); v++) {
+            // reverse so that adjacency list is in same order as original
+            Stack<Edge> reverse = new Stack<Edge>();
+            for (Edge e : G.adj[v]) {
+                reverse.push(e);
+            }
+            for (Edge e : reverse) {
+                adj[v].add(e);
+            }
+        }
+    }
+
+
+    /**
+     * Returns the number of vertices in this edge-weighted graph.
+     *
+     * @return the number of vertices in this edge-weighted graph
+     */
+    public int V() {
+        return V;
+    }
+
+    /**
+     * Returns the number of edges in this edge-weighted graph.
+     *
+     * @return the number of edges in this edge-weighted graph
+     */
+    public int E() {
+        return E;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    /**
+     * Adds the undirected edge {@code e} to this edge-weighted graph.
+     *
+     * @param  e the edge
+     * @throws IllegalArgumentException unless both endpoints are between {@code 0} and {@code V-1}
+     */
+    public void addEdge(Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(e);
+        adj[w].add(e);
+        E++;
+    }
+
+    /**
+     * Returns the edges incident on vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the edges incident on vertex {@code v} as an Iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<Edge> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    /**
+     * Returns the degree of vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the degree of vertex {@code v}               
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int degree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+    /**
+     * Returns all edges in this edge-weighted graph.
+     * To iterate over the edges in this edge-weighted graph, use foreach notation:
+     * {@code for (Edge e : G.edges())}.
+     *
+     * @return all edges in this edge-weighted graph, as an iterable
+     */
+    public Iterable<Edge> edges() {
+        Bag<Edge> list = new Bag<Edge>();
+        for (int v = 0; v < V; v++) {
+            int selfLoops = 0;
+            for (Edge e : adj(v)) {
+                if (e.other(v) > v) {
+                    list.add(e);
+                }
+                // add only one copy of each self loop (self loops will be consecutive)
+                else if (e.other(v) == v) {
+                    if (selfLoops % 2 == 0) list.add(e);
+                    selfLoops++;
+                }
+            }
+        }
+        return list;
+    }
+
+    /**
+     * Returns a string representation of the edge-weighted graph.
+     * This method takes time proportional to <em>E</em> + <em>V</em>.
+     *
+     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     *         followed by the <em>V</em> adjacency lists of edges
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " " + E + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (Edge e : adj[v]) {
+                s.append(e + "  ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+
+    public static void main(String[] args) {
+		// TODO
+        EdgeWeightedGraph ewg = new EdgeWeightedGraph(2);
+        ewg.setCoordinates(new double[][]{{5, 6}, {8, 10}});
+        System.out.println(Arrays.deepToString(ewg.getCoordinates()));
+    }
+
+}
+
